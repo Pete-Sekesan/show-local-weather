ea7b3d4771aa87a71c1d8c071bd09b9bf2df440f
Peter Sekesan <psekesan@gmail.com>
2021-02-20 13:54:16 -0500

    index updates

--- index.js
+++ index.js
@@ -10,9 +10,44 @@ const apiKey = "bfb8a7a5a080b6de3711a504201eb15f";
 
 //get data from API
 
-async function getWeather() {
-  const response = await fetch(`${weatherURL}11566&APPID=${apiKey}`);
+async function getWeather(zip) {
+  const response = await fetch(`${weatherURL}${zipCode}&APPID=${apiKey}`);
   const data = await response.json();
   console.log(data);
 }
-getWeather();
+
+//generate input form
+function createForm() {
+  return `
+  <form id = entry-form>
+  <div id = entry-form>
+  <label for="zip-search">Enter Zip Code</label>
+  <input type="text" name="zip-search" id="zip-search" required
+  placeholder="90210">
+
+  <input type="submit" value="Get Weather">
+
+
+  </div>
+  </form>
+   `;
+}
+
+//display results
+function displayResults(data) {}
+
+//render the  created form
+function renderForm() {
+  $("main").append(createForm());
+  return;
+}
+
+//watch form
+function watchForm() {
+  $("form").submit((event) => {
+    event.preventDefault();
+    const zipCode = $("#zip-search").val();
+  });
+}
+
+renderForm();
