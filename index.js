adf8bddd446c7fd18f340e399f273c9d7f7c0de5
Peter Sekesan <psekesan@gmail.com>
2021-02-20 14:41:34 -0500

    adjusted form to html

--- index.js
+++ index.js
@@ -10,31 +10,20 @@ const apiKey = "bfb8a7a5a080b6de3711a504201eb15f";
 
 //get data from API
 
-async function getWeather(zip) {
+async function getWeather(zipCode) {
   const response = await fetch(`${weatherURL}${zipCode}&APPID=${apiKey}`);
   const data = await response.json();
   console.log(data);
 }
 
-//generate input form
-function createForm() {
-  return `
-  <form id = entry-form>
-  <div id = entry-form>
-  <label for="zip-search">Enter Zip Code</label>
-  <input type="text" name="zip-search" id="zip-search" required
-  placeholder="90210">
-
-  <input type="submit" value="Get Weather">
-
-
-  </div>
-  </form>
-   `;
-}
-
 //display results
-function displayResults(data) {}
+function displayResults(data) {
+  $("main").append(
+    `<div> 
+    ${data}
+    </div>`
+  );
+}
 
 //render the  created form
 function renderForm() {
@@ -47,7 +36,9 @@ function watchForm() {
   $("form").submit((event) => {
     event.preventDefault();
     const zipCode = $("#zip-search").val();
+    getWeather(zipCode);
+    displayResults();
   });
 }
 
-renderForm();
+$(watchForm);
